049121822a7bf1cf968e6d7a9dd9f497a131a663
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-09 23:58:49 -0400

    set function to set state onClick

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -6,7 +6,7 @@ const [count, setCount] = useState(0)
   return (
     <div> 
       <h2>{count}</h2>
-      <button>Click</button>
+      <button onClick={() => setCount(count + 1)}>Click</button>
     </div>
   )
 }
